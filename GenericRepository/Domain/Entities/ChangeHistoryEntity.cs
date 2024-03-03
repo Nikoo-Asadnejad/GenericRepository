6529acb713aec0c9aa681d1d446d12bbdf222f56
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-03-03 21:39:46 +0330

    refactor : remove extra id from entities

--- GenericRepository/Domain/Entities/ChangeHistoryEntity.cs
+++ GenericRepository/Domain/Entities/ChangeHistoryEntity.cs
@@ -2,9 +2,11 @@ namespace GenericRepository.Domain.Entities;
 
 public class ChangeHistoryEntity : BaseEntity
 {
-    public long Id { get; set; }
-    public string OldValue { get; set; }
-    public string NewValue { get; set; }
-    public string RelatedEntityId { get; set; }
-    public string RelatedEntityType { get; set; }
+
+    public string OldValue { get; private set; }
+    
+    public string NewValue { get;private set; }
+    
+    public string RelatedEntityId { get; private set; }
+    public string RelatedEntityType { get; private set; }
 }
--- GenericRepository/Domain/Entities/OutBoxMessage.cs
+++ GenericRepository/Domain/Entities/OutBoxMessage.cs
@@ -9,14 +9,12 @@ public class OutBoxMessage : BaseEntity
         Type = type;
         Content = content;
     }
-
     public OutBoxMessage(IDomainEvent domainEvent)
     {
         Type = domainEvent.GetType().Name;
         Content = JsonSerializer.Serialize(domainEvent);
     }
-    
-    public long Id { get; private set; }
+
     public string Type { get; private set; }
     public string Content { get; private set; }
 }
