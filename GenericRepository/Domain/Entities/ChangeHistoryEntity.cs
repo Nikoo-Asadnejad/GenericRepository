dc89b2d70f09714087814b4ec5fc6c22a56a217f
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-03-03 21:45:17 +0330

    feat(changeLogHistory): add logHistory

--- GenericRepository/Domain/Entities/ChangeHistoryEntity.cs
+++ GenericRepository/Domain/Entities/ChangeHistoryEntity.cs
@@ -1,12 +1,23 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace GenericRepository.Domain.Entities;
 
 public class ChangeHistoryEntity : BaseEntity
 {
-
     public string OldValue { get; private set; }
     
     public string NewValue { get;private set; }
     
     public string RelatedEntityId { get; private set; }
+    
     public string RelatedEntityType { get; private set; }
+
+    public void LogHistory(BaseEntity oldEntity , BaseEntity newEntity)
+    {
+        OldValue = JsonSerializer.Serialize(oldEntity);
+        NewValue = JsonSerializer.Serialize(oldEntity);
+        RelatedEntityId = oldEntity.Id.ToString();
+        RelatedEntityType = oldEntity.GetType().Name;
+    }
 }
