5ef05f822b3a73fe0c040ccc72f48eb735c35ce0
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-11-07 12:53:02 +0330

    chore: implemnt value object

--- GenericRepository/Domain/ValueObject.cs
+++ GenericRepository/Domain/ValueObject.cs
@@ -1,6 +1,35 @@
 namespace GenericRepository.Domain;
 
-public class ValueObject
+public abstract class ValueObject
 {
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(1, (current, obj) =>
+            {
+                return HashCode.Combine(current, obj);
+            });
+    }
     
+    protected abstract IEnumerable<object> GetEqualityComponents();
 }
--- GenericRepository/Domain/ValueObjects/OutBoxMessageEventType.cs
+++ GenericRepository/Domain/ValueObjects/OutBoxMessageEventType.cs
@@ -1,7 +1,14 @@
+using GenericRepository.Domain.Enums;
+
 namespace GenericRepository.Domain.ValueObjects;
 
 public class OutBoxMessageEventType : ValueObject
 {
+    public EventTypeEnum EventType { get; init; }
     
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        throw new NotImplementedException();
+    }
 }
 
